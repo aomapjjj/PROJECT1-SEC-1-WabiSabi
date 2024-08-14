a78c8093a65ebd51db52692b2f66bb18ce86c48a
Panipak Jongsirivanich <panipak.jong@mail.kmutt.ac.th>
2024-08-15 00:44:10 +0700

    setup-createGame

---
+++ bingo-game/src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+ 
+<template>
+  <div class="w-full h-screen">
+    <div class=" bg-gradient-to-r from-sky-500 to-indigo-500 w-full h-full"> 
+      <!-- Header -->
+      <div class="flex flex-row justify-center">
+        <!-- <img src="../src/assets/img/logo.png"> -->
+        <h1 class="text-3xl text-white font-bold m-10">Bingo Game</h1>
+      </div>
+     
+    </div>
+   
+  </div>
+
+  
+
+</template>
+ 
+<style scoped>
+
+</style>
