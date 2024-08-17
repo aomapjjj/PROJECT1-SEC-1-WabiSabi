082ea5ad989b8dd13ae53e60dbb17074672f74c3
OParnu <110712221+OParnu@users.noreply.github.com>
2024-08-17 19:31:49 +0700

    update numbers collection

--- bingo-game/src/App.vue
+++ bingo-game/src/App.vue
@@ -1,13 +1,8 @@
 <script setup>
 import { ref, onMounted } from "vue"
-const numbers = ref([
-  1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22,
-  23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41,
-  42, 43, 44, 45, 46, 47, 48, 49, 50
-])
+const numbers = ref(Array.from(Array(51).keys()).splice(1))
 const usedNumber = ref([])
 const randomBtnText = ref("Random Number")
-
 const randomNumber = () => {
   let randomIndex = Math.floor(Math.random() * numbers.value.length)
   let number = numbers.value.splice(randomIndex, 1)[0]
