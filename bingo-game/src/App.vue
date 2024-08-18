95d871aede7489b7f8d1b4b5e39f73dd975aff27
Panipak Jongsirivanich <panipak.jong@mail.kmutt.ac.th>
2024-08-18 20:33:31 +0700

    Add Audio in BG

--- bingo-game/src/App.vue
+++ bingo-game/src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, onMounted } from "vue"
+import bgSoundUrl from "./assets/audio/bg-sound.mp3"
 
 const numbers = ref(Array.from(Array(51).keys()).splice(1))
 const usedNumber = ref([])
@@ -7,19 +8,22 @@ const randomBtnText = ref("Random Number")
 const toDisabledwhileRandom = ref(false)
 const shuffledNumbers = ref([])
 const selectedNumbers = ref([])
+const showAudio = ref(true)
+
+onMounted(() => {
+  shuffleNumber()
+})
 
 const randomNumber = () => {
   let randomIndex = Math.floor(Math.random() * numbers.value.length)
   let number = numbers.value.splice(randomIndex, 1)[0]
   usedNumber.value.push(number)
   if (numbers.value.length === 0) randomBtnText.value = "Out Of Number!"
-  toDisabledwhileRandom.value = true;
+  toDisabledwhileRandom.value = true
 }
 
 let numberOnBoard = Array.apply(null, { length: 51 }).map(Number.call, Number)
 numberOnBoard.shift()
-
-
 
 const shuffleNumber = () => {
   let currentIndex = numberOnBoard.length,
@@ -29,14 +33,13 @@ const shuffleNumber = () => {
     randomIndex = Math.floor(Math.random() * currentIndex)
     currentIndex--
 
-
-     [numberOnBoard[currentIndex], numberOnBoard[randomIndex]] = [
+    ;[numberOnBoard[currentIndex], numberOnBoard[randomIndex]] = [
       numberOnBoard[randomIndex],
       numberOnBoard[currentIndex]
     ]
   }
   shuffledNumbers.value = [...numberOnBoard]
-  toDisabledwhileRandom.value = false;
+  toDisabledwhileRandom.value = false
 }
 
 console.log(numberOnBoard)
@@ -44,7 +47,9 @@ console.log(numberOnBoard)
 const toggleSelection = (number) => {
   if (number === usedNumber.value[usedNumber.value.length - 1]) {
     if (selectedNumbers.value.includes(number)) {
-      selectedNumbers.value = selectedNumbers.value.filter((num) => num !== number)
+      selectedNumbers.value = selectedNumbers.value.filter(
+        (num) => num !== number
+      )
     } else {
       selectedNumbers.value.push(number)
     }
@@ -53,24 +58,42 @@ const toggleSelection = (number) => {
 
 const isSelected = (number) => selectedNumbers.value.includes(number)
 
-onMounted(() => {
-  shuffleNumber()
-})
+let audio = new Audio(bgSoundUrl)
+
+
+const playMusic = () => {
+  audio.play()
+  showAudio.value = !showAudio.value
+}
+
+
+const stopMusic = () => {
+  audio.pause()
+  showAudio.value = !showAudio.value
+}
 
 console.log(shuffleNumber())
-
 </script>
 
 <template>
   <div class="w-full h-full">
     <div class="bg-gradient-to-r from-sky-500 to-indigo-500 w-full h-full">
+
+      <div class="flex float-end mt-6 m-3">
+        <button @click="playMusic" v-show="showAudio">
+          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="32" viewBox="0 0 576 512"><path fill="#ffffff" d="M215.03 71.05L126.06 160H24c-13.26 0-24 10.74-24 24v144c0 13.25 10.74 24 24 24h102.06l88.97 88.95c15.03 15.03 40.97 4.47 40.97-16.97V88.02c0-21.46-25.96-31.98-40.97-16.97m233.32-51.08c-11.17-7.33-26.18-4.24-33.51 6.95c-7.34 11.17-4.22 26.18 6.95 33.51c66.27 43.49 105.82 116.6 105.82 195.58S488.06 408.1 421.79 451.59c-11.17 7.32-14.29 22.34-6.95 33.5c7.04 10.71 21.93 14.56 33.51 6.95C528.27 439.58 576 351.33 576 256S528.27 72.43 448.35 19.97M480 256c0-63.53-32.06-121.94-85.77-156.24c-11.19-7.14-26.03-3.82-33.12 7.46s-3.78 26.21 7.41 33.36C408.27 165.97 432 209.11 432 256s-23.73 90.03-63.48 115.42c-11.19 7.14-14.5 22.07-7.41 33.36c6.51 10.36 21.12 15.14 33.12 7.46C447.94 377.94 480 319.54 480 256m-141.77-76.87c-11.58-6.33-26.19-2.16-32.61 9.45c-6.39 11.61-2.16 26.2 9.45 32.61C327.98 228.28 336 241.63 336 256c0 14.38-8.02 27.72-20.92 34.81c-11.61 6.41-15.84 21-9.45 32.61c6.43 11.66 21.05 15.8 32.61 9.45c28.23-15.55 45.77-45 45.77-76.88s-17.54-61.32-45.78-76.86"/></svg></button>
+        <button @click="stopMusic" v-show="!showAudio" >
+          <svg xmlns="http://www.w3.org/2000/svg" width="36" height="32" viewBox="0 0 640 512"><path d="M633.82 458.1l-69-53.33C592.42 360.8 608 309.68 608 256c0-95.33-47.73-183.58-127.65-236.03-11.17-7.33-26.18-4.24-33.51 6.95-7.34 11.17-4.22 26.18 6.95 33.51 66.27 43.49 105.82 116.6 105.82 195.58 0 42.78-11.96 83.59-33.22 119.06l-38.12-29.46C503.49 318.68 512 288.06 512 256c0-63.09-32.06-122.09-85.77-156.16-11.19-7.09-26.03-3.8-33.12 7.41-7.09 11.2-3.78 26.03 7.41 33.13C440.27 165.59 464 209.44 464 256c0 21.21-5.03 41.57-14.2 59.88l-39.56-30.58c3.38-9.35 5.76-19.07 5.76-29.3 0-31.88-17.53-61.33-45.77-76.88-11.58-6.33-26.19-2.16-32.61 9.45-6.39 11.61-2.16 26.2 9.45 32.61 11.76 6.46 19.12 18.18 20.4 31.06L288 190.82V88.02c0-21.46-25.96-31.98-40.97-16.97l-49.71 49.7L45.47 3.37C38.49-2.05 28.43-.8 23.01 6.18L3.37 31.45C-2.05 38.42-.8 48.47 6.18 53.9l588.36 454.73c6.98 5.43 17.03 4.17 22.46-2.81l19.64-25.27c5.41-6.97 4.16-17.02-2.82-22.45zM32 184v144c0 13.25 10.74 24 24 24h102.06l88.97 88.95c15.03 15.03 40.97 4.47 40.97-16.97V352.6L43.76 163.84C36.86 168.05 32 175.32 32 184z" fill="#ffffff"/></svg>
+        </button>
+      </div>
+
+
       <!-- Header -->
       <div class="flex flex-row justify-center">
-        <img class="h-40 w-40" src="../src/assets/img/bingo.png" />
+        <img class="h-40 w-40 ml-24" src="../src/assets/img/bingo.png" />
 
         <!-- <img src="../src/assets/img/logo.png"> -->
         <!-- <h1 class="text-3xl text-white font-bold m-10">Bingo Game</h1> -->
-
       </div>
 
       <div class="flex flex-col items-center">
@@ -84,13 +107,16 @@ console.log(shuffleNumber())
       <div class="flex flex-row justify-center m-8">
         <button
           class="btn mr-3"
-          :disabled="randomBtnText === 'Out Of Number!' "
-          @click="randomNumber "
-          
+          :disabled="randomBtnText === 'Out Of Number!'"
+          @click="randomNumber"
         >
           {{ randomBtnText }}
         </button>
-        <button class="btn btn-warning" @click="shuffleNumber" :disabled="toDisabledwhileRandom" >
+        <button
+          class="btn btn-warning"
+          @click="shuffleNumber"
+          :disabled="toDisabledwhileRandom"
+        >
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width="32"
@@ -131,7 +157,6 @@ console.log(shuffleNumber())
         </div>
       </div>
 
-
       <!-- Bingo Board -->
 
       <div class="overflow-x-auto flex justify-center">
@@ -154,17 +179,17 @@ console.log(shuffleNumber())
                   v-for="j in 5"
                   :key="j"
                   :id="shuffledNumbers[(i - 1) * 5 + (j - 1)]?.toString()"
-
-                  @click="toggleSelection(shuffledNumbers[(i - 1) * 5 + (j - 1)])"
+                  @click="
+                    toggleSelection(shuffledNumbers[(i - 1) * 5 + (j - 1)])
+                  "
                   :class="[
                     'h-20 w-20 text-center cursor-pointer',
                     isSelected(shuffledNumbers[(i - 1) * 5 + (j - 1)])
                       ? 'bg-pink-500 text-white'
                       : '',
-                    shuffledNumbers[(i - 1) * 5 + (j - 1)] === usedNumber[usedNumber.length - 1]
-                     
+                    shuffledNumbers[(i - 1) * 5 + (j - 1)] ===
+                      usedNumber[usedNumber.length - 1]
                   ]"
-
                 >
                   {{ shuffledNumbers[(i - 1) * 5 + (j - 1)] }}
                 </td>
